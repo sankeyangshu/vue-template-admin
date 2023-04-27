47b69125fcad2d187fd9ad0b2d2b0e25542a5c98
sankeyangshu <1260323835@qq.com>
2023-04-27 15:55:54 +0800

    feat: 新增用户信息组件

---
+++ src/layouts/components/NavBar/components/User.vue
@@ -0,0 +1,56 @@
+<template>
+  <el-dropdown class="user" trigger="click">
+    <div class="avatar-dropdown">
+      <el-avatar
+        shape="square"
+        :size="36"
+        class="avatar"
+        src="https://img.yzcdn.cn/vant/cat.jpeg"
+      />
+      <div class="user-name">用户名</div>
+      <el-icon class="up-down">
+        <ArrowDown />
+      </el-icon>
+    </div>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item>
+          <el-icon> <Edit /> </el-icon>
+          <span>修改密码</span>
+        </el-dropdown-item>
+        <el-dropdown-item divided>
+          <el-icon> <SwitchButton /> </el-icon>
+          <span>退出登陆</span>
+        </el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.el-dropdown-link {
+  cursor: pointer;
+  //color: var(--el-color-primary);
+  display: flex;
+  align-items: center;
+}
+.user {
+  cursor: pointer;
+  .avatar-dropdown {
+    height: 50px;
+    padding: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .avatar {
+      margin-right: 6px;
+    }
+    .user-name {
+      position: relative;
+      margin: 0 6px;
+    }
+  }
+}
+</style>
--- src/layouts/components/NavBar/index.vue
+++ src/layouts/components/NavBar/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>navbar</div>
+  <div>
+    <!-- 用户信息 开始 -->
+    <User />
+    <!-- 用户信息 结束 -->
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import User from './components/User.vue';
+</script>
 
 <style lang="scss" scoped></style>
