c870956e5b238a46b72f85e72d055369e4d8a9bc
sankeyangshu <1260323835@qq.com>
2023-09-27 09:04:55 +0800

    chore: 新增eslint导入导出规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,11 +16,46 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
   },
-  plugins: ['vue', '@typescript-eslint'],
+  plugins: ['vue', '@typescript-eslint', 'simple-import-sort'],
   rules: {
     '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/promise-function-async': 'off',
     'vue/multi-word-component-names': 'off',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          [
+            // 以字母(或数字或下划线)或“@”后面跟着字母开头的东西,通常为内置模块引入
+            '^@?\\w',
+            // 内部导入 "@/"
+            '^@(/.*|$)',
+            `^@/assets$`,
+            `^@/components$`,
+            `^@/config$`,
+            `^@/hooks$`,
+            `^@/plugins$`,
+            `^@/router$`,
+            `^@/store$`,
+            `^@/styles$`,
+            `^@/utils$`,
+            // 父级导入. 把 `..` 放在最后.
+            '^\\.\\.(?!/?$)',
+            '^\\.\\./?$',
+            // 同级导入. 把同一个文件夹.放在最后
+            '^\\./(?=.*/)(?!/?$)',
+            '^\\.(?!/?$)',
+            '^\\./?$',
+            // 样式导入.
+            '^.+\\.?(css|less|scss)$',
+            // 带有副作用导入，比如import 'a.css'这种.
+            '^\\u0000',
+          ],
+        ],
+      },
+    ],
+    'simple-import-sort/exports': 'error', // 导出
+    'import/order': 'off',
   },
 };
